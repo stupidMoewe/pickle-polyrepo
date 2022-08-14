81e760fa014241c1cd710bb455244d6bd0ecdf37
stupidMoewe <martintefra@gmail.com>
2022-08-14 11:49:27 -0400

    Updates

--- common/src/events/question-created-event.ts
+++ common/src/events/question-created-event.ts
@@ -1,12 +1,24 @@
 import { Subjects } from "./subjects";
 
+enum questionType {
+	TextText,
+	TextImage,
+	ImageText,
+	ImageImage,
+	VideoText,
+	VideoImage,
+}
+
 export interface QuestionCreatedEvent {
 	subject: Subjects.QuestionCreated;
 	data: {
 		id: string;
-		title: string;
+		questionType: questionType;
+		title: string; // whether the text of the title or the url of the image or the video
 		answer1: string;
 		answer2: string;
+		answer3?: string;
+		answer4?: string;
 		creatorId: string;
 	};
 }
