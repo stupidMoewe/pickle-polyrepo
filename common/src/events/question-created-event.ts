ba26e62510b0a7f6c4bc67acab5b92d689c00ff4
stupidMoewe <martintefra@gmail.com>
2022-09-15 09:13:47 -0400

    Updates

--- common/src/events/question-created-event.ts
+++ common/src/events/question-created-event.ts
@@ -1,18 +1,9 @@
 import { Subjects } from "./subjects";
 
-export type QuestionTypeOptions =
-	| "TextText"
-	| "TextImage"
-	| "ImageText"
-	| "ImageImage"
-	| "VideoText"
-	| "VideoImage";
-	
 export interface QuestionCreatedEvent {
 	subject: Subjects.QuestionCreated;
 	data: {
 		id: string;
-		questionType: QuestionTypeOptions;
 		creatorId: string;
 		expirationDate: number;
 	};
